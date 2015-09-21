8ee09e8f4962f4ccc32aec2ddd69104a1b8eb751
Francesco Zoffoli <makers.f.dev@gmail.com>
2015-09-21 15:54:53 +0200

    Printing that we reached the callback even if there is no callback

--- TeamComp/main.py
+++ TeamComp/main.py
@@ -54,10 +54,10 @@ def download_matches(store_callback, seed_players, minimum_tier = Tier.bronze,
                      prints_on=False, minimum_match_id=0):
 
     def checkpoint(time_slice, players_to_analyze, total_matches, max_match_id):
-        if end_of_time_slice_callback:
-            if prints_on:
-                print("{} - Calling end_of_time callback with {} downloaded matches"
+        if prints_on:
+                print("{} - Reached the checkpoint."
                       .format(datetime.datetime.now().strftime("%m-%d %H:%M"), total_matches))
+        if end_of_time_slice_callback:
             end_of_time_slice_callback(datetime.datetime.utcfromtimestamp(time_slice.end/1000), players_to_analyze, total_matches, max_match_id)
 
     players_to_analyze = TierSeed(tiers=leagues_by_summoner_ids(seed_players, queue))

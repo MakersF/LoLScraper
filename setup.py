cfa35f13e006b86b2cc4335d89c9978779cf0a88
Francesco Zoffoli <makers.f.dev@gmail.com>
2015-12-28 12:06:24 +0800

    v0.0.04

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 project_url = "https://github.com/MakersF/LoLScraper"
 setup(
     name="lol_scraper",
-    version="0.0.03",
+    version="0.0.04",
     author="Francesco Zoffoli",
     author_email="makers.f.dev@gmail.com",
     url=project_url,
